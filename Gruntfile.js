742958509cd8fca5b01e79fd555286c3b40e0d1d
James Abley <james.abley@gmail.com>
2014-08-11 11:23:42 +0100

    Add scsslint to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,11 +45,22 @@ module.exports = function(grunt) {
           style: 'nested',
         }
       },
+    },
+    scsslint: {
+      allFiles: [
+        'stylesheets/**/*.scss',
+      ],
+      options: {
+        bundleExec: false,
+        reporterOutput: 'scss-lint-report.xml',
+        colorizeOutput: true
+      },
     }
   });
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('test', ['sass', 'clean', 'jasmine']);
+  grunt.loadNpmTasks('grunt-scss-lint');
+  grunt.registerTask('test', ['scsslint', 'sass', 'clean', 'jasmine']);
   grunt.registerTask('default', ['test']);
 };
